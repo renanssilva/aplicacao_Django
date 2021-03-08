5f49d02bf762f648f3a84082277ce13e987874c6
Renan <renan.silva.soares.br@gmail.com>
2021-03-08 16:57:15 -0300

    Funcionalidades de menssagens de ducesso e erro, e refatoração do código

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,40 +1,40 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User  # Vamos usar o USER para verificar se já existe um usuario(modelo) cadastrado
-from django.contrib import auth  # auth -> user authentication(autenticação de usuario)
+from django.contrib import auth, messages  # auth -> user authentication(autenticação de usuario)
 
 from receitas.models import Receita
 
 
 def cadastro(request):
     if request.method == 'POST':
-
         nome = request.POST['nome']
         email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
-
         if not nome.strip():
-            print('O campo nome não pode ficar em branco')
+            messages.error(request, 'O campo nome não pode ficar em branco')
             return redirect('cadastro')
-
-        if not email.strip():
-            print('O campo email não pode ficar em branco')
+        if campo_vazio(nome):
+            messages.error(request,'O campo nome não pode ficar em branco')
             return redirect('cadastro')
-
-        if senha != senha2:
-            print('As senhas não são iguais')
+        if campo_vazio(email):
+            messages.error(request,'O campo email não pode ficar em branco')
             return redirect('cadastro')
-
+        if senhas_nao_sao_iguais(senha, senha2):
+            messages.error(request, 'As senhas não são iguais')
+            return redirect('cadastro')
         if User.objects.filter(email=email).exists():
-            print('Usuario já cadastrado')
+            messages.error(request, 'Email já cadastrado')
             return redirect('cadastro')
-
+        if User.objects.filter(username=nome).exists():
+            messages.error(request, 'Usuário já cadastrado')
+            return redirect('cadastro')
         user = User.objects.create_user(username=nome, email=email, password=senha)
         user.save()
-        print('Usuario cadastrado com sucesso')
+        print('Usuário cadastrado com sucesso')
         return redirect('login')
     else:
-        return render(request, 'usuarios/cadastro.html')
+        return render(request,'usuarios/cadastro.html')
 
 
 def login(request):
@@ -42,14 +42,14 @@ def login(request):
         email = request.POST['email']
         senha = request.POST['senha']
 
-        if email == "" or senha == "":
-            print('Os compos email e senha não podem ficar em branco')
+        if campo_vazio(email) or campo_vazio(senha):
+            messages.error(request, 'Os compos email e senha não podem ficar em branco')
             return redirect('login')
 
         if User.objects.filter(email=email).exists():
             nome = User.objects.filter(email=email).values_list('username', flat=True).get()
             user = auth.authenticate(request, username=nome, password=senha)
-            print(user)
+
             if user is not None:
                 auth.login(request, user)
                 print('Login realizado com sucesso')
@@ -97,3 +97,11 @@ def cria_receita(request):
         return redirect('dashboard')
     else:
         return render(request, 'usuarios/cria_receita.html')
+
+
+def campo_vazio(campo):
+    return not campo.strip()
+
+def senhas_nao_sao_iguais(senha, senha2):
+    return senha != senha2
+
