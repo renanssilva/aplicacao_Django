7ae2f80c8476ea50cf7033bed0afb03f5aaa098e
Renan <renan.silva.soares.br@gmail.com>
2021-03-08 12:14:36 -0300

    Funcionalidade de autenticação de usuario e direcionamento para dashboard

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User  # Vamos usar o USER para verificar se já existe um usuario cadastrado
+from django.contrib.auth.models import User  # Vamos usar o USER para verificar se já existe um usuario(modelo) cadastrado
+from django.contrib import auth  # auth -> user authentication(autenticação de usuario)
 
 
 def cadastro(request):
@@ -26,7 +27,7 @@ def cadastro(request):
             print('Usuario já cadastrado')
             return redirect('cadastro')
 
-        user =  User.objects.create_user(username=nome, email=email, password=senha)
+        user = User.objects.create_user(username=nome, email=email, password=senha)
         user.save()
         print('Usuario cadastrado com sucesso')
         return redirect('login')
@@ -35,12 +36,32 @@ def cadastro(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+
+        if email == "" or senha == "":
+            print('Os compos email e senha não podem ficar em branco')
+            return redirect('login')
+
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            print(user)
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso')
+                return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 
 def dashboard(request):
-    pass
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+    else:
+        return redirect('index')
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
